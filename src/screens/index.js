8c63eb62c8524619f4ebbb2425651e766f8ae38e
UssefBhl <ybhl83@gmail.com>
2022-06-10 12:08:44 +0100

    📦 grouped screens

---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import LoginScreen from "./LoginScreen";
+import PremiumScreen from "./PremiumScreen";
+import SignUpScreen from "./SignUpScreen";
+import StayDetailsScreen from "./StayDetailsScreen";
+import StaysScreen from "./StaysScreen";
+import TourDetailsScreen from "./TourDetailsScreen";
+import MainScreen from "./Main/MainScreen";
+
+export {LoginScreen, PremiumScreen, SignUpScreen, StayDetailsScreen, StaysScreen, TourDetailsScreen, MainScreen}

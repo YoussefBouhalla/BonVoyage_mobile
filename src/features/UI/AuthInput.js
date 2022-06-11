ca37b02633fb0c1aefc1810139d120892d626ec8
UssefBhl <ybhl83@gmail.com>
2022-06-11 12:11:29 +0100

    💄 login screen UI

--- src/features/UI/AuthInput.js
+++ src/features/UI/AuthInput.js
@@ -1,12 +1,85 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {StyleSheet, View, Dimensions, TextInput, Text } from 'react-native'
 import React from 'react'
+import { useFonts } from 'expo-font';
 
-export default function AuthInput() {
+const {width, height} = Dimensions.get('window');
+
+
+export default function AuthInput({ marginTop, Icon, placeholder, error, errorType }) {
+
+  // loading Poppins fonts
+  const [loaded] = useFonts({
+    'Poppins-Bold': require('../../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-Medium': require('../../assets/fonts/Poppins-Medium.ttf'),
+  });
+
+  if (!loaded) {
+      return null;
+  }
+
   return (
-    <View>
-      <Text>AuthInput</Text>
-    </View>
+    <>
+      <View style={{...styles.input_holder , marginTop}}>
+        <View style={{padding: 5, backgroundColor: '#a9a9a95e' , borderRadius: 999}}>
+            <Icon />
+        </View>
+        <TextInput
+            style={styles.input}
+            // onChangeText={}
+            placeholder={placeholder}
+        />
+      </View>
+      <Text style={{...styles.error_text, display: error[errorType] ? 'flex' : 'none'}}>{error[errorType]}</Text> 
+    </>
+  
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  shadow : {
+    shadowColor: "#a1a0a0",
+    shadowOffset: {
+        width: 0,
+        height: 2,
+    },
+    shadowOpacity:  0.17,
+    shadowRadius: 2.54,
+    elevation: 3
+  },
+  input_holder: {
+    display: 'flex',
+    flexDirection: 'row',
+    overflow: 'hidden',
+    backgroundColor: '#F5F5F5',
+    borderStyle: "solid",
+    borderColor: "#a9a9a95e",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    height: 60,
+    padding: 10,
+    alignItems: 'center',
+    shadowColor: "#a1a0a0",
+    shadowOffset: {
+        width: 0,
+        height: 2,
+    },
+    shadowOpacity:  0.17,
+    shadowRadius: 2.54,
+    elevation: 3,
+    marginHorizontal: 15
+  },
+  input: {
+    height: '100%',
+    width: '100%',
+    fontSize: 18,
+    fontFamily: "Poppins-Medium",
+    marginLeft: 10
+  },
+  error_text: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 14,
+    paddingTop: 5,
+    color: '#bd1a1a',
+    marginHorizontal: 15
+}
+})
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, NativeModules, View, Dimensions, Pressable } from 'react-native'
 import React from 'react'
+import { useFonts } from 'expo-font';
 
-export default function LoginScreen() {
+import EmailIcon from '../assets/icons/email.svg'
+import KeyHoleIcon from '../assets/icons/keyhole.svg'
+import FacebookIcon from '../assets/icons/facebookAuth.svg'
+import GoogleIcon from '../assets/icons/googleAuth.svg'
+
+const {width, height} = Dimensions.get('window');
+
+const {StatusBarManager} = NativeModules;
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBarManager.HEIGHT;
+
+import Logo from '../assets/icons/AuthLogo.svg';
+import AuthInput from '../features/UI/AuthInput';
+import AuthButton from '../features/UI/AuthButton';
+
+export default function LoginScreen({navigation}) {
+
+
+  // loading Poppins fonts
+  const [loaded] = useFonts({
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+  });
+
+  if (!loaded) {
+      return null;
+  }
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <View style={{alignItems: 'center', width, marginTop: STATUSBAR_HEIGHT +15 }}>
+        <Logo />
+        <Text style={{color: '#2E2E2E', fontFamily: 'Poppins-Bold', fontSize: 20 , marginTop: 14}}>Welcome back!</Text>
+        <Text style={{color: '#2E2E2E', fontFamily: 'Poppins-Medium', fontSize: 18 }}>We are happy to have you back !</Text>
+      </View>
+
+      <View style={{width, display: 'flex' , flexDirection: 'column',marginTop: 30}}>
+        <AuthInput marginTop ={0} Icon={EmailIcon} placeholder="Email" error={{}} errorType="email" />
+        <AuthInput marginTop ={15} Icon={KeyHoleIcon} placeholder="Password" error={{}} errorType="password" />
+      </View>
+
+      <View style={{width}}>
+        <Text style={{marginVertical: 15, marginLeft: 15, color: "#EB5353", fontFamily: 'Poppins-Medium',textDecorationLine: 'underline', fontSize: 16}}>Forgot password?</Text>  
+      </View>
+
+      <AuthButton content="login"/>
+      
+      <View style={{width, justifyContent: "center", marginTop: 15, display: 'flex' , flexDirection: 'row'}}>
+        <Text style={{color: "#2E2E2E", fontFamily: 'Poppins-Medium', fontSize: 16}}>You don't have an account ? </Text>
+        <Text style={{color: "#EB5353", fontFamily: 'Poppins-Medium',textDecorationLine: 'underline', fontSize: 16}}>Sign up</Text>
+      </View>
+ 
+      <View style={{width: width - 30, height: 1 , backgroundColor: "#A3A3A3" , margin:15}}>
+        <Text style={{paddingHorizontal: 10,backgroundColor: "#F8F4F1",color: "#2E2E2E",position: 'absolute', transform: [{ translateY: -12 }, { translateX: (width - 50)/2}], fontFamily: 'Poppins-Medium', fontSize: 16}}>or</Text>
+      </View>
+
+      <Text style={{color: "#2E2E2E", fontFamily: 'Poppins-Medium', fontSize: 16}}>Social Media Login</Text>
+
+      <View style={{width, justifyContent: "center", marginTop: 15, display: 'flex' , flexDirection: 'row'}}>
+        <Pressable style={{padding: 22, backgroundColor: "#3B5998", borderRadius: 999}}>
+          <FacebookIcon />
+        </Pressable>
+        <Pressable style={{padding: 22,marginLeft: 15, backgroundColor: "#EA4335", borderRadius: 999}}>
+          <GoogleIcon />
+        </Pressable>
+      </View>
+
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    height,
+    width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F8F4F1',
+  }
+})

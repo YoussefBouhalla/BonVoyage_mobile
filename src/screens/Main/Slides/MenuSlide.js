1b0930f793978e404bb40deb629dff85572764bb
UssefBhl <ybhl83@gmail.com>
2022-06-15 11:47:12 +0100

    🔥 profile card

--- src/screens/Main/Slides/MenuSlide.js
+++ src/screens/Main/Slides/MenuSlide.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View, Dimensions} from 'react-native'
 import React from 'react'
+import ProfileCard from '../../../features/UI/ProfileCard';
 
 const {width, height} = Dimensions.get('window');
 
 export default function MenuSlide() {
   return (
     <View style={{flex: 1, width}}>
-      <Text>MenuSlide</Text>
+        <ProfileCard />
+        <Text style={{marginHorizontal: 15, marginVertical: 10,fontFamily: 'Poppins-Bold', fontSize: 18 }}>Pages</Text>
     </View>
   )
 }
